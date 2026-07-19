9cf2a482ebf822fc9aedaa3601355f77c9be7729
Sonlis <bastien.jeannelle@gmail.com>
2026-07-19 18:00:37 +0200

    fix: Kanshi format

--- modules/home-manager/background/programs/kanshi.nix
+++ modules/home-manager/background/programs/kanshi.nix
@@ -2,31 +2,35 @@
 {
   services.kanshi = {
     enable = true;
-    settings = {
-      no_laptop_screen_when_connected = {
-        name = "no_laptop_screen_when_connected";
-        outputs = [
-          {
-            criteria = "DP-3";
-            status = "enable";
-            position = "0,0";
-          }
-          {
-            criteria = "eDP-1";
-            status = "disable";
-          }
-        ];
-      };
-      just_laptop = {
-        name = "just_laptopt";
-        outputs = [
-          {
-            criteria = "eDP-1";
-            status = "enable";
-            position = "0,0";
-          }
-        ];
-      };
-    };
+    settings = [
+      {
+        profile = {
+          name = "Laptop with screen connected";
+          outputs = [
+            {
+              criteria = "DP-3";
+              status = "enable";
+              position = "0,0";
+            }
+            {
+              criteria = "eDP-1";
+              status = "disable";
+            }
+          ];
+        };
+      }
+      {
+        profile = {
+          name = "Just laptop";
+          outputs = [
+            {
+              criteria = "eDP-1";
+              status = "enable";
+              position = "0,0";
+            }
+          ];
+        };
+      }
+    ];
   };
 }
