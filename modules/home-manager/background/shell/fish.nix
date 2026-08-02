3fecbd94ce01096df101cd036a0ac62cca4e12f2
Sonlis <bastien.jeannelle@gmail.com>
2026-08-02 10:19:23 +0200

    feat: Support both light and dark theme

--- modules/home-manager/background/shell/fish.nix
+++ modules/home-manager/background/shell/fish.nix
@@ -30,58 +30,77 @@ in
       enable = true;
       target = "fish/themes/everforest.theme";
       text = ''
-        # syntax highlighting variables
-        # https://fishshell.com/docs/current/interactive.html#syntax-highlighting-variables
-        fish_color_normal d3c6ab
-        fish_color_command d699b6
-        fish_color_keyword 83c092
-        fish_color_quote dbbc7f
-        fish_color_redirection 7a8e8e
-        fish_color_end 5a6b6b
-        fish_color_error e67e80
-        fish_color_param e69875
-        fish_color_comment 9da9a9
-        # fish_color_match --background=brblue
+        [light]
+        fish_color_normal 5c6a72
+        fish_color_command df69ba
+        fish_color_keyword 35a77c
+        fish_color_quote 8da101
+        fish_color_redirection 3a94c5
+        fish_color_end a6b0a0
+        fish_color_error f85552
+        fish_color_param f57d26
+        fish_color_comment 829181
         fish_color_selection --reverse
-        # fish_color_history_current --bold
-        fish_color_operator d3c6ab
-        fish_color_escape 5a6b6b
-        fish_color_autosuggestion 9da9a9
-        fish_color_cwd e69875
-        # fish_color_cwd_root red
-        fish_color_user dbbc7f
-        fish_color_host 83c092
-        fish_color_host_remote d699b6
-        fish_color_cancel d3c6ab
-        fish_color_search_match --background=dbbc7f
+        fish_color_operator 5c6a72
+        fish_color_escape a6b0a0
+        fish_color_autosuggestion 829181
+        fish_color_cwd f57d26
+        fish_color_user dfa000
+        fish_color_host 35a77c
+        fish_color_host_remote df69ba
+        fish_color_cancel 5c6a72
+        fish_color_search_match --background=dfa000
         fish_color_valid_path
 
-        # pager color variables
-        # https://fishshell.com/docs/current/interactive.html#pager-color-variables
-        fish_pager_color_progress e69875
-        fish_pager_color_background --background=2b3a39
-        fish_pager_color_prefix 83c092
-        fish_pager_color_completion 9da9a9
-        fish_pager_color_description 9da9a9
+        fish_pager_color_progress f57d26
+        fish_pager_color_background --background=fffbef
+        fish_pager_color_prefix 35a77c
+        fish_pager_color_completion 829181
+        fish_pager_color_description 829181
         fish_pager_color_secondary_background
         fish_pager_color_secondary_prefix
         fish_pager_color_secondary_completion
         fish_pager_color_secondary_description
-        fish_pager_color_selected_background --background=34454a
-        fish_pager_color_selected_prefix 83c092
-        fish_pager_color_selected_completion d3c6ab
-        fish_pager_color_selected_description d3c6ab
+        fish_pager_color_selected_background --background=ecf5ed
+        fish_pager_color_selected_prefix 35a77c
+        fish_pager_color_selected_completion 5c6a72
+        fish_pager_color_selected_description 5c6a72
 
-        # custom color variables
-        fish_color_subtle 9da9a9
-        fish_color_text d3c6ab
-        fish_color_love e67e80
-        fish_color_gold dbbc7f
-        fish_color_rose e69875
-        fish_color_pine 5a6b6b
-        fish_color_foam 83c092
-        fish_color_iris d699b6
-        fish_color_base 2b3a39
+        [dark]
+        fish_color_normal d3c6aa
+        fish_color_command d699b6
+        fish_color_keyword 83c092
+        fish_color_quote a7c080
+        fish_color_redirection 7fbbb3
+        fish_color_end 7a8478
+        fish_color_error e67e80
+        fish_color_param e69875
+        fish_color_comment 9da9a0
+        fish_color_selection --reverse
+        fish_color_operator d3c6aa
+        fish_color_escape 7a8478
+        fish_color_autosuggestion 9da9a0
+        fish_color_cwd e69875
+        fish_color_user dbbc7f
+        fish_color_host 83c092
+        fish_color_host_remote d699b6
+        fish_color_cancel d3c6aa
+        fish_color_search_match --background=dbbc7f
+        fish_color_valid_path
+
+        fish_pager_color_progress e69875
+        fish_pager_color_background --background=272e33
+        fish_pager_color_prefix 83c092
+        fish_pager_color_completion 9da9a0
+        fish_pager_color_description 9da9a0
+        fish_pager_color_secondary_background
+        fish_pager_color_secondary_prefix
+        fish_pager_color_secondary_completion
+        fish_pager_color_secondary_description
+        fish_pager_color_selected_background --background=384b55
+        fish_pager_color_selected_prefix 83c092
+        fish_pager_color_selected_completion d3c6aa
+        fish_pager_color_selected_description d3c6aa
       '';
     };
 
@@ -89,6 +108,7 @@ in
       enable = true;
       target = "fish/themes/rose-pine.theme";
       text = ''
+        [dark]
         # syntax highlighting variables
         # https://fishshell.com/docs/current/interactive.html#syntax-highlighting-variables
         fish_color_normal e0def4
@@ -141,6 +161,60 @@ in
         fish_color_foam 9ccfd8
         fish_color_iris c4a7e7
         fish_color_base 191724
+
+        [light]
+        # syntax highlighting variables
+        # https://fishshell.com/docs/current/interactive.html#syntax-highlighting-variables
+        fish_color_normal 575279
+        fish_color_command 907aa9
+        fish_color_keyword 56949f
+        fish_color_quote ea9d34
+        fish_color_redirection 286983
+        fish_color_end 797593
+        fish_color_error b4637a
+        fish_color_param d7827e
+        fish_color_comment 797593
+        # fish_color_match --background=brblue
+        fish_color_selection --reverse
+        # fish_color_history_current --bold
+        fish_color_operator 575279
+        fish_color_escape 286983
+        fish_color_autosuggestion 797593
+        fish_color_cwd d7827e
+        # fish_color_cwd_root red
+        fish_color_user ea9d34
+        fish_color_host 56949f
+        fish_color_host_remote 907aa9
+        fish_color_cancel 575279
+        fish_color_search_match --background=faf4ed
+        fish_color_valid_path
+
+        # pager color variables
+        # https://fishshell.com/docs/current/interactive.html#pager-color-variables
+        fish_pager_color_progress d7827e
+        fish_pager_color_background --background=fffaf3
+        fish_pager_color_prefix 56949f
+        fish_pager_color_completion 797593
+        fish_pager_color_description 797593
+        fish_pager_color_secondary_background
+        fish_pager_color_secondary_prefix
+        fish_pager_color_secondary_completion
+        fish_pager_color_secondary_description
+        fish_pager_color_selected_background --background=f2e9e1
+        fish_pager_color_selected_prefix 56949f
+        fish_pager_color_selected_completion 575279
+        fish_pager_color_selected_description 575279
+
+        # custom color variables
+        fish_color_subtle 797593
+        fish_color_text 575279
+        fish_color_love b4637a
+        fish_color_gold ea9d34
+        fish_color_rose d7827e
+        fish_color_pine 286983
+        fish_color_foam 56949f
+        fish_color_iris 907aa9
+        fish_color_base faf4ed
       '';
     };
   };
