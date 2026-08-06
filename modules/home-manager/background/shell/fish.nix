e7b8068a4451cac5c8a4f458d997fa0bdb0dd2ed
Sonlis <bastien.jeannelle@gmail.com>
2026-08-06 22:21:45 +0200

    feat: only pokemon from sinnoh

--- modules/home-manager/background/shell/fish.nix
+++ modules/home-manager/background/shell/fish.nix
@@ -23,7 +23,7 @@ in
       interactiveShellInit = ''
         fish_config theme choose "${config.theme}"
         zoxide init fish | source
-        pokeget --hide-name random
+        pokeget --hide-name sinnoh
         set fish_greeting
       '';
     };
